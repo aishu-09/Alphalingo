f0cf322f9496cdb304783d588e83800a5532473f
aishu <ajathan@students.cs.ubc.ca>
2026-09-19 17:16:23 -0300

    Added cards

---
+++ app/cards.ts
@@ -0,0 +1,7 @@
+export type Card = { id: number; front: string; back: string };
+
+export const cards: Card[] = [
+  { id: 1, front: "Hello", back: "Bonjour" },
+  { id: 2, front: "To eat", back: "Manger" },
+  { id: 3, front: "Today", back: "Aujourd'hui" },
+];
